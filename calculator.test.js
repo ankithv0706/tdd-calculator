9b6a26040287e849dc7dd793056d9a4332cfdd7c
Ankith Vishwakarma <ankithv0706@gmail.com>
2025-07-05 19:45:33 +0530

    test case to check if calculator contains add function

--- calculator.test.js
+++ calculator.test.js
@@ -2,4 +2,8 @@ const Calculator = require("./calculator");
 
 const calculator = new Calculator();
 
-describe("TDD Calculator", () => {});
+describe("TDD Calculator", () => {
+  test("should contain add function", () => {
+    expect(typeof calculator.add === "function").toBe(true);
+  });
+});
